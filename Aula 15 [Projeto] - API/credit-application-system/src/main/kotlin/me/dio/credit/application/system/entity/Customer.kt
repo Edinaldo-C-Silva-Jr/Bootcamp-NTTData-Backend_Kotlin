ce3db014030e978ce5a21ebaed1d62c4fab2108b
Edinaldo-C-Silva-Jr <edinaldo.csilvajr@outlook.com>
2024-02-22 17:08:44 -0300

    Adiciona as Anotações JPA para a criação de tabelas no banco de dados. Também configura o arquivo application.yml.

--- Aula 15 [Projeto] - API/credit-application-system/src/main/kotlin/me/dio/credit/application/system/entity/Customer.kt
+++ Aula 15 [Projeto] - API/credit-application-system/src/main/kotlin/me/dio/credit/application/system/entity/Customer.kt
@@ -1,12 +1,17 @@
 package me.dio.credit.application.system.entity
 
+import jakarta.persistence.*
+
+@Entity
+@Table(name = "Customer")
 data class Customer(
-    var firstName: String = "",
-    var lastName: String = "",
-    val cpf: String,
-    var email: String = "",
-    var password: String = "",
-    var address: Address = Address(),
+    @Column(nullable = false) var firstName: String = "",
+    @Column(nullable = false) var lastName: String = "",
+    @Column(nullable = false) val cpf: String,
+    @Column(nullable = false) var email: String = "",
+    @Column(nullable = false) var password: String = "",
+    @Column(nullable = false) @Embedded var address: Address = Address(),
+    @Column(nullable = false) @OneToMany(fetch = FetchType.LAZY, cascade = [CascadeType.REMOVE], mappedBy = "customer")
     var credits: List<Credit> = mutableListOf(),
-    val id: Long? = null
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY) val id: Long? = null
 )
